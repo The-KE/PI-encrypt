dc687b98e3eb2e48ae0fd85ee97caf4cd7e84946
The-KE <kareemelayyan388@gmail.com>
2026-04-01 19:34:31 -0500

    Fix a bunch of bugs and optimize the code

--- main.c
+++ main.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
 #include <stdlib.h>
 
+#define PI_STR "314159265358979"
 #define PI_LENGTH 15
 
-long double pi = 3.14159265358979L;
-int StrungPi[15];
+int StrungPi[PI_LENGTH];
 
 void arrayifyPi() {
-  char piTemp[16];
-  sprintf(piTemp, "%Lf", pi);
-  int rahhh = 0;
-  for (int i=0; i < 0; i++;) {
-    char numStr = piTemp[i];
-    if (numStr == ".") {
-      continue;
+    const char *piTemp = PI_STR;
+    for (int i = 0; i < PI_LENGTH; i++) {
+        StrungPi[i] = piTemp[i] - '0';
     }
-    else {
-      int numToAdd = numStr - "0";
-      StrungPi[rahhh] = numToAdd;
-      rahhh++;
-    }
-  }
 }
 
-int main(int argc, char *argv[]){
-  printf("π🔒 Pi Encrypt\n");
-  printf("By UnknownKE/The KE\n");
-  printf("Made for Instructables spring 2026 All Things Pi contest\n\n");
-  printf("Doing unimportant stuff you prob dont care about...\n"};
-  arrayifyPi();
+int main(int argc, char *argv[]) {
+    printf("π🔒 Pi Encrypt\n");
+    printf("By UnknownKE/The KE\n");
+    printf("Made for Instructables spring 2026 All Things Pi contest\n\n");
+    printf("Doing unimportant stuff you prob dont care about...\n");
+    arrayifyPi();
+    return 0;
 }
