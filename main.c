0a7336af1ae57781b637777bfcdde9c4f4011300
The-KE <kareemelayyan388@gmail.com>
2026-04-26 14:17:55 -0500

    Update ktofile condition and fix outputFSname print
    Fix the condition for ktofile and correct outputFSname printf.

--- main.c
+++ main.c
@@ -78,10 +78,9 @@ unsigned char *ReadFileAndKeyStuff(char fring[]) {
         FILE *urand = fopen("/dev/urandom", "r");
         fread(&RandKey, 1, sizeof(RandKey), urand);
         fclose(urand);
-        /*if (ktofile == true) {
+        if (ktofile == true) {
             FILE *kout = fopen("piEncKey.txt", "r+");
         }
-        */
         return fuf;
     }
     unsigned char *fuf = mmap(NULL, bytes.st_size, PROT_READ, MAP_PRIVATE, desc, 0);
@@ -110,9 +109,10 @@ void arrayifyArgs(int argc, char *argv[]) {
   strncpy(inputFSname, argv[4], sizeof(inputFSname)-2);
   inputFSname[sizeof(inputFSname)-1] = '\0';
   strncpy(outputFSname, argv[5], sizeof(outputFSname)-2);
+  printf("%s", outputFSnane
   outputFSname[sizeof(inputFSname)-1] = '\0';
-  //if (argc >= 6 && !strcmp(argv[6], "-k"))
-   //   ktofile = true;
+  if (argc >= 7 && !strcmp(argv[6], "-k"))
+      ktofile = true;
 }
   void printKey(const char *str) {
     for (int i = 0; str[i] != '\0'; i++) {
